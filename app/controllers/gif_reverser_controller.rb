a16f780822bec8f747f1bb19dd6da74093451de7
Daniel Jamrozik <djamro2@illinois.edu>
2017-04-18 16:30:56 -0500

    can reverse images now

--- app/controllers/gif_reverser_controller.rb
+++ app/controllers/gif_reverser_controller.rb
@@ -1,21 +1,51 @@
 
-require "open-uri"
+# good test image to use for reversing:
+# https://i.redd.it/0vbby789bcsy.gif
 
-def download_gif(url)
-	open(url) {|f|
-		puts f.content_type
-	    File.open("whatever_file.gif","wb") do |file|
+require "open-uri"
+require "securerandom"
+
+def delete_file(filename)
+
+end
+
+def download_gif(url, name="output")
+	open(url) { |f|
+
+		# determine file extension, complete name
+		file_extension = ".gif"
+		if f.content_type == "image/gif"
+			file_extension = ".gif"
+		end
+		filename = name + file_extension
+
+		# open and write
+	    File.open(filename, "wb") do |file|
 	    	file.puts f.read
 	    end
+
+	    return file_extension
 	}
 end
 
-def run_command()
-	puts `echo hello world`
+def get_random_string
+	return SecureRandom.urlsafe_base64 8
+end
+
+def run_reverse_command(raw_name, file_extension)
+	filename_normal = raw_name + file_extension
+	filename_reversed = raw_name + "_reversed" + file_extension
+	system "convert #{filename_normal} -coalesce -reverse  -quiet -layers OptimizePlus  -loop 0 #{filename_reversed}"
 end
 
 def validate_url(gif_url)
-	return gif_url.index('.com')
+	if not (gif_url.index('.com') or gif_url.index('.it'))
+		return nil
+	end
+	if gif_url.index('.gifv')
+		return nil
+	end
+	return true
 end
 
 class GifReverserController < ApplicationController
@@ -31,15 +61,22 @@ class GifReverserController < ApplicationController
 		# get gif_url and validate
 		gif_url = params[:gif_url]
 		if not validate_url(gif_url)
-			error_msg = {:message => "Invalide url"}
+			error_msg = {:message => "Invalide gif url"}
 			render :status => 500, :json => error_msg and return
 		end
 
+		# used for saving locally w/o name conflicts
+		raw_name = get_random_string
+
 		# download gif from url
-		download_gif(gif_url)
+		file_extension = download_gif(gif_url, raw_name)
 
 		# run command line to reverse downloaded file
-		run_command()
+		run_reverse_command raw_name, file_extension
+
+		# upload to S3
+
+		# delete the file
 
 		# return message
 		msg = {:response => "OK"}
--- public/app/controllers/GifReverserController.js
+++ public/app/controllers/GifReverserController.js
@@ -3,21 +3,27 @@ app.controller('GifReverserController', ['$scope', 'GifReverserService', functio
 
     // status message
     $scope.errorMessage = false;
-    $scope.successfulReverse = false
+    $scope.successfulReverse = false;
+    $scope.status = {
+        inProgess: false
+    };
 
     vm.init = function() {
         // init controller here
     };
 
     $scope.reverseGif = function(gifUrl) {
+        $scope.status.inProgress = true;
     	var params = {
     		gif_url: gifUrl
     	};
 
     	GifReverserService.reverseGif(params, function(result){
             $scope.errorMessage = false;
+            $scope.status.inProgress = false;
             $scope.successfulReverse = true;
     	}, function(error) {	
+            $scope.status.inProgress = false;
     		if (error && error.data && error.data.message) {
                 $scope.errorMessage = error.data.message;
             }
