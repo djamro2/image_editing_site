2cc053ee99167041ada65a363950978f8911d72c
Daniel Jamrozik <djamro2@illinois.edu>
2017-04-25 16:05:33 -0500

    back to original CORS with rack-cors

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,13 @@ module ImageEditingSite
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.action_dispatch.default_headers = {
-	    'Access-Control-Allow-Origin' => '*',
-	    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",") # or whatever else you would like to allow
-	}
+    # allow cors
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
 
   end
 end
